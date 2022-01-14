ca8d5870a25b42c99521bd384b3b10d9c44d8120
lhh <1285393562@qq.com>
2022-01-14 16:22:37 +0800

    1

---
+++ src/test/java/com/itlhh/MptestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itlhh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MptestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
